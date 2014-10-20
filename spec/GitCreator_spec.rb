066fb7a2823cc5415d5a09a06de040bbd990f711
Despot <Despot@Despot>
2014-10-20 21:52:44 +0100

    add in even more rspec

--- spec/GitCreator_spec.rb
+++ spec/GitCreator_spec.rb
@@ -1,15 +1,27 @@
 require 'fileutils'
 require_relative 'spec_helper'
 
-
 describe GitRecordCreator, "Git Repository" do
-    it "is created when git_repo does not exists" do
-      allow(GitRecordCreator).to receive(:git_repository_exists?){false}
-      expect {GitRecordCreator.new}.to output(/creating/).to_stdout
-    end
-    it "is not created when git_repo exists"
-    after(:all) do
-      FileUtils.rm_rf('./configuration') if Dir.exists?('./configuration')
-    end
+
+  let(:git_record){GitRecordCreator.new}
+  before(:all) do
+    FileUtils.rm_rf('./configuration') if Dir.exists?('./configuration')
+  end
+
+  it "is created when git_repo does not exist" do
+    allow(git_record).to receive(:git_repository_exists?){false}
+    expect {git_record.to output(/creating/).to_stdout}
+    expect(git_record.instance_variable_get(:@g)).to_not eq(nil)
+  end
+
+  it "is not created when git_repo exists" do 
+    allow(git_record).to receive(:git_repository_exists?){true}
+    expect {git_record.to_not output(/creating/).to_stdout}
+    expect(git_record.instance_variable_get(:@g)).to eq(nil)
+  end
+
+  after(:all) do
+    FileUtils.rm_rf('./configuration') if Dir.exists?('./configuration')
+  end
 
 end
