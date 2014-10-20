6ceeda8bfa910ac41d9f30e220c77385da0ec1e2
Despot <Despot@Despot>
2014-10-20 21:25:37 +0100

    update rspec file

--- spec/GitCreator_spec.rb
+++ spec/GitCreator_spec.rb
@@ -1,1 +1,15 @@
-require spec_helper
+require 'fileutils'
+require_relative 'spec_helper'
+
+
+describe GitRecordCreator, "Git Repository" do
+    it "is created when git_repo does not exists" do
+      allow(GitRecordCreator).to receive(:git_repository_exists?){false}
+      expect {GitRecordCreator.new}.to output(/creating/).to_stdout
+    end
+    it "is not created when git_repo exists"
+    after(:all) do
+      FileUtils.rm_rf('./configuration') if Dir.exists?('./configuration')
+    end
+
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,2 @@
-require_relative '../src'
+require_relative '../src/GitCreator'
 
