62e2bc58bd920bfb6532a639bac446faa8d0b262
Despot <Despot@Despot>
2014-10-20 20:02:55 +0100

    update spec skeleton

---
+++ src/GitCreator.rb
@@ -0,0 +1,16 @@
+require 'rubygems'
+require 'git'
+
+class GitRecordCreator
+  def initialize
+  end
+
+  private 
+
+  def git_repository_exists?
+  end 
+
+  def create_git_repository
+
+  end
+end
