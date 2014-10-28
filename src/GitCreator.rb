503fe31a574d40a519aa4ad37b71049a166459ce
Despot <Despot@Despot>
2014-10-28 22:42:39 +0000

    final commit for night

--- src/GitCreator.rb
+++ src/GitCreator.rb
@@ -1,10 +1,10 @@
 require 'rubygems'
 require 'git'
 require 'logger'
-
+require 'pp'
 class GitRecordCreator
   def initialize
-    Dir.mkdir('./configuration') if not Dir.exists?('./configuration')
+    Dir.mkdir('./configuration') if not Dir.exists?('./configuration/')
     if not git_repository_exists?
       puts "Git repo does not exist, creating"
       create_git_repository
@@ -17,18 +17,29 @@ class GitRecordCreator
       commit_file(config_file) 
   return true
   end
-
+  
   private 
 
   def git_repository_exists?
     begin
-      Git.open('configuration')
+      @g = Git.open('configuration')
     rescue ArgumentError 
       return false
     end
     true
   end 
 
+  def get_commit_message
+    if previous_commit_exists?
+      commit_message = @g.gcommit(get_latest_log).message
+    else
+      "No message"
+    end
+  end
+
+  def get_latest_log
+    return @g.log[0]
+  end
   def create_git_repository
     @g = Git.init('configuration')
     @g.config('user.name', 'User')
@@ -41,18 +52,33 @@ class GitRecordCreator
 
   def commit_file(file)
     git_add(file)
-    git_commit('bland')
+    git_commit("#{Time.now}, config.yaml")
+  end
+  
+  def previous_commit_exists?
+    !(@g.branches.to_s ==  '')
   end
 
   def git_add(file)
     puts "Adding #{file} to index"
     @g.add(file)
   end
-
+  
+  def get_diff_message
+    if @g.log.size > 1
+       puts @g.diff(@g.log[1], @g.log[0])
+       puts @g.diff(@g.log[1], @g.log[0]).stats
+    end
+  end
   def git_commit(message)
     puts "Commiting"
-     @g.commit(message)
+    @g.commit(message)
+   puts get_commit_message
+
+   get_diff_message
+
   end
+
 end
 
 
